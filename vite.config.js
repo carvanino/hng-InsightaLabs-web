c9cf370d1db787133a54a7dfdf500bf4a00d9172
Akinola <akinolatofunmi04@gmail.com>
2026-04-30 16:57:25 +0100

    fix host

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const { HOST } = process.env;
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       // Proxy API and auth calls to backend during development
-      "/api":        "http://localhost:3004",
-      "/auth":       "http://localhost:3004",
-      "/csrf-token": "http://localhost:3004",
+      "/api": `http://${HOST}:3004`,
+      "/auth": `http://${HOST}:3004`,
+      "/csrf-token": `http://${HOST}:3004`,
     },
   },
 });
